5efe5e2df5aeae6432e55b40468f7f0b9c4649ff
Ash <ashnamuh@gmail.com>
2020-04-09 21:38:37 +0900

    add a styled-components

--- src/components/AshComponent.tsx
+++ src/components/AshComponent.tsx
@@ -1,11 +1,18 @@
 import * as React from 'react'
+import styled from 'styled-components'
 
 interface Props {
   title: string;
 }
 
+const S = {
+  container: styled.p`
+    color: yellow;
+  `
+}
+
 export default function AshComponent({ title }: Props) {
   return (
-    <p>{title}</p>
+    <S.container>{title}</S.container>
   )
 }

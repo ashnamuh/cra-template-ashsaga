a1b4538fd3122998c3c54efb1903c7a21a0bc361
Ash <ashnamuh@gmail.com>
2020-04-09 21:47:56 +0900

    add a store with Redux and Redux-saga

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import {
   Link,
 } from "react-router-dom";
 import GlobalStyled from 'styles/global'
+import { Provider } from 'react-redux'
+import store from 'services/store'
 
 import IndexPage from 'pages/index'
 import AshPage from 'pages/ash'
@@ -13,27 +15,31 @@ import ManduPage from 'pages/mandu'
 
 export default function App() {
   return (
-    <Router>
+    <>
       <GlobalStyled />
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/ash">Ash</Link>
-          </li>
-          <li>
-            <Link to="/mandu">Mandu</Link>
-          </li>
-        </ul>
+      <Provider store={store}>
+        <Router>
+          <div>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/ash">Ash</Link>
+              </li>
+              <li>
+                <Link to="/mandu">Mandu</Link>
+              </li>
+            </ul>
 
-        <Switch>
-          <Route exact path="/" component={IndexPage} />
-          <Route exact path="/ash" component={AshPage} />
-          <Route exact path="/mandu" component={ManduPage} />
-        </Switch>
-      </div>
-    </Router>
+            <Switch>
+              <Route exact path="/" component={IndexPage} />
+              <Route exact path="/ash" component={AshPage} />
+              <Route exact path="/mandu" component={ManduPage} />
+            </Switch>
+          </div>
+        </Router>
+      </Provider>
+    </>
   );
 }

786ca4d953735ac1df16821e639f8db8933ff06d
Ash <ashnamuh@gmail.com>
2020-05-12 13:55:23 +0900

    modify loading status

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,3 @@
 /// <reference types="react-scripts" />
+
+type LoadingStatus = 'initial' | 'loading' | 'loaded'
--- src/services/article/reducers.ts
+++ src/services/article/reducers.ts
@@ -7,18 +7,18 @@ export type Actions = ActionType<typeof actions>
 export interface ArticleState {
   articles: {
     data: Article[];
-    loading: boolean;
+    loading: LoadingStatus;
   };
 }
 
 const initialState: ArticleState = {
   articles: {
     data: [],
-    loading: false
+    loading: 'initial'
   }
 }
 
 export default createReducer<ArticleState, Actions>(initialState)
-  .handleAction(actions.fetchArticleListAsync.request, (state) => ({ ...state,  articles: { ...state.articles, loading: true } }))
-  .handleAction(actions.fetchArticleListAsync.success, (state, action) => ({ ...state,  articles: { data: action.payload, loading: false } }))
-  .handleAction(actions.fetchArticleListAsync.failure, (state) => ({ ...state,  articles: { ...state.articles, loading: false } }))
+  .handleAction(actions.fetchArticleListAsync.request, (state) => ({ ...state,  articles: { ...state.articles, loading: 'loading' } }))
+  .handleAction(actions.fetchArticleListAsync.success, (state, action) => ({ ...state,  articles: { data: action.payload, loading: 'loaded' } }))
+  .handleAction(actions.fetchArticleListAsync.failure, (state) => ({ ...state,  articles: { ...state.articles, loading: 'loaded' } }))
